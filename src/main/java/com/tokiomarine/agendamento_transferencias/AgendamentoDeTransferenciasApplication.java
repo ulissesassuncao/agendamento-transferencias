b559dae28d257aad19962a5f3d94ae83f6f94795
Ulisses Assuncao <ulisses.silva14@gmail.com>
2025-02-15 13:54:37 -0300

    Commit Inicial: Criação do Projeto agendamento-transferencias e sincronização com repositório remoto

---
+++ src/main/java/com/tokiomarine/agendamento_transferencias/AgendamentoDeTransferenciasApplication.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.agendamento_transferencias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeTransferenciasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeTransferenciasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tokiomarine/agendamento_transferencias/AgendamentoDeTransferenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.agendamento_transferencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeTransferenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
